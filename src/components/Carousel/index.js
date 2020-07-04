686150b8a1742d824b3576fd5108bd8d237d0ca2
Mario Souto <soutomarios@gmail.com>
2020-07-04 18:36:36 -0300

    Update

--- src/components/Carousel/index.js
+++ src/components/Carousel/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { VideoCardGroupContainer, VideoCardList, Title, ExtraLink } from './styles';
+import { VideoCardGroupContainer, Title, ExtraLink } from './styles';
 import VideoCard from './components/VideoCard';
 import Slider, { SliderItem } from './components/Slider';
 
