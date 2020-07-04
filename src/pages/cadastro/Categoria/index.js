c9ee7b7fa622bfd44df1d522f4d6e8647c47157f
Mario Souto <soutomarios@gmail.com>
2020-07-04 16:54:02 -0300

    Update aula 02

--- src/pages/cadastro/Categoria/index.js
+++ src/pages/cadastro/Categoria/index.js
@@ -7,7 +7,19 @@ function CadastroCategoria() {
     <PageDefault>
       <h1>Cadastro de Categoria</h1>
 
-      
+      <form>
+
+        <label>
+          Nome da Categoria:
+          <input
+            type="text"
+          />
+        </label>
+
+        <button>
+          Cadastrar
+        </button>
+      </form>
 
 
       <Link to="/">
